ec71790b576593ddbcef6c153a37f86f793c1c36
rrhiemstar <rrhiemstar@gmail.com>
2024-03-20 12:47:22 +0100

    <release> TerraUnit..v0.2.0

--- Project.lua
+++ Project.lua
@@ -1,8 +1,9 @@
 Project = {
-    name = "TerraUnit",
-    uuid = "410448b0-e356-4f60-a72f-5dadcb388172",
-    authors = {"rrhiemstar<rrhiemstar@gmail.com>"},
-    version = "0.1.0",
-    deps = {}
+  name = "TerraUnit",
+  uuid = "410448b0-e356-4f60-a72f-5dadcb388172",
+  authors = {"rrhiemstar<rrhiemstar@gmail.com>", }, 
+  version = "0.2.0",
+  deps = {
+    },
 }
 return Project
