f5f5d04c2a06292cc195d7e9f294da661dd7a237
rrhiemstar <rrhiemstar@gmail.com>
2024-03-21 22:36:48 +0100

    <project> added language key to project file

--- Project.lua
+++ Project.lua
@@ -2,6 +2,7 @@ Project = {
   name = "TerraUnit",
   uuid = "410448b0-e356-4f60-a72f-5dadcb388172",
   authors = {"rrhiemstar<rrhiemstar@gmail.com>", }, 
+  language = "terra",
   version = "0.2.0",
   deps = {
     },
